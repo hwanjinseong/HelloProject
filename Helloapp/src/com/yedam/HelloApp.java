bc4268b74c7cc101f22c53afca0e2c270128b5c1
hwanjinseong <134985112+hwanjinseong@users.noreply.github.com>
2023-05-30 11:50:46 +0900

    Update HelloApp.java

--- Helloapp/src/com/yedam/HelloApp.java
+++ Helloapp/src/com/yedam/HelloApp.java
@@ -8,7 +8,21 @@ public class HelloApp {
 		int result = num1 + num2;
 		result = num1 - num3;
 		System.out.println("결과값:" + result);
-
+	
+		//변수: 변하는 값을 저장, 컴퓨터의 메모리에 저장 공간
+		int num1; //음의 정수, 0 , 양의 정수
+		num = 100;
+		num1 = -100;
+		
+		double num2 = -1.2;
+		num2 = 12.3;
+		num2 = 123.0; //정수 -> 실수 내부적으로 형변환.
+		
+		double result = 0;
+		result = num1 + num2;
+		
+		double num1 = -1. 2; //변수의 범위 int < double.
+		System.out.println("결과: " + result);
 	} // end of main.
 } // end of class.
 //src 소스파일 bin 실행되는파일
