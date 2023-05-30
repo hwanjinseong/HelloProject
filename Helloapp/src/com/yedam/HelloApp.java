6075bfe92aea01c63ef795a6bac9f5bd01e65ff9
admin <admin@DESKTOP-M3G86US>
2023-05-30 10:43:30 +0900

    first commit.

---
+++ Helloapp/src/com/yedam/HelloApp.java
@@ -0,0 +1,15 @@
+package com.yedam;
+
+public class HelloApp {
+	public static void main(String[] args) {
+		int num1 = 100;
+		int num2 = 50;
+		int num3 = 10; //추가.
+		int result = num1 + num2;
+		result = num1 - num3;
+		System.out.println("결과값:" + result);
+
+	} // end of main.
+} // end of class.
+//src 소스파일 bin 실행되는파일
+
---
+++ Helloapp/src/com/yedam/Helloworld.java
@@ -0,0 +1,8 @@
+package com.yedam;
+
+public class Helloworld {
+	//main 메소드 시작.
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	}
+}
